c06bd8b62db60c4bb6133381cb05d6db6067cd26
xyyuhans <xyyuhans@hotmail.com>
2021-02-02 11:03:00 +0800

    auto install package

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,5 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 
+;; auto install packages
+;; https://emacs.stackexchange.com/questions/28932/how-to-automate-installation-of-packages-with-emacs-file
+(dolist (package '(use-package))
+  (unless (package-installed-p package)
+    (package-install package)))
+
 (provide 'init-local)
 ;;; init-local.el ends here
