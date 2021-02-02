27a63d8d062738ea24bac58b5648c009f2fc98d6
xyyuhans <xyyuhans@hotmail.com>
2021-02-02 11:07:31 +0800

    org mode setting

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -31,5 +31,26 @@
 ;; https://emacs.stackexchange.com/questions/19344/why-does-xdg-open-not-work-in-eshell
 (setq process-connection-type nil)
 
+;; org mode
+(setq org-default-notes-file "~/Nutstore Files/sync/orgmode/todo.org")
+(setq org-agenda-files '("~/Nutstore Files/sync/orgmode" "~/Nutstore Files/sync/orgmode/hidden" "~/Syncthing/backup/org"))
+(setq org-archive-location "~/Nutstore Files/sync/orgmode/archive::")
+;; auto truncate
+;; https://superuser.com/questions/299886/linewrap-in-org-mode-of-emacs/299897
+(setq org-startup-truncated nil)
+;; set tag interval to 0
+;; https://emacs.stackexchange.com/questions/10524/too-much-whitespace-on-setting-org-mode-tags
+(setq org-tags-column 0)
+;; orgmode don't set repeat task to NEXT
+(setq org-todo-repeat-to-state nil)
+;; show superscript
+(setq org-pretty-entities 1)
+;; org agenda sort
+(setq org-agenda-sorting-strategy
+      '((agenda time-up priority-down habit-down user-defined-up effort-up category-keep)
+        (todo category-up effort-up)
+        (tags category-up effort-up)
+        (search category-up)))
+
 (provide 'init-local)
 ;;; init-local.el ends here
