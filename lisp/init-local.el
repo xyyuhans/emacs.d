19d607a2a44a42305db29242db1ab6e83d1140de
xyyuhans <xyyuhans@hotmail.com>
2021-02-02 11:05:06 +0800

    other config
    - start server
    - change file auto save location
    - email address
    - spell check
    - auto reload
    - make xdg-open works

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -8,5 +8,28 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+;; start server to use emacsclient -n
+(server-start)
+
+;; place all auto save files in a directory
+;; https://emacs.stackexchange.com/questions/17210/how-to-place-all-auto-save-files-in-a-directory
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/auto-save-list/" t)))
+
+;; email address
+;; https://emacs.stackexchange.com/questions/26434/how-to-change-email-address-in-org-mode-publish-postamble
+(setq user-mail-address "xyyuhans@hotmail.com")
+
+;; spell check
+(setq ispell-local-dictionary "english")
+
+;; auto reload files
+;; https://stackoverflow.com/questions/1480572/how-to-have-emacs-auto-refresh-all-buffers-when-files-have-changed-on-disk
+(global-auto-revert-mode t)
+
+;; make xdg-open works
+;; https://emacs.stackexchange.com/questions/19344/why-does-xdg-open-not-work-in-eshell
+(setq process-connection-type nil)
+
 (provide 'init-local)
 ;;; init-local.el ends here
