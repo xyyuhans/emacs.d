ffffc607212a0b4b65c3d50a2fa7d8aaa4cef791
xyyuhans <xyyuhans@hotmail.com>
2021-03-07 11:35:41 +0800

    Revert "eaf"
    This reverts commit 425759798e383b9b4a9bc5e041681c7717e6ee8a.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -217,23 +217,5 @@
   ;; no global mode now
   (add-hook 'org-mode-hook 'real-auto-save-mode))
 
-(use-package eaf
-  ;; don't use aur now, install dependency by shell
-  ;; https://github.com/manateelazycat/emacs-application-framework/issues/557
-  :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-  :init
-  (use-package epc :defer t :ensure t)
-  (use-package ctable :defer t :ensure t)
-  (use-package deferred :defer t :ensure t)
-  (use-package s :defer t :ensure t)
-  :custom
-  (eaf-browser-continue-where-left-off t)
-  :config
-  (eaf-setq eaf-browser-enable-adblocker "true")
-  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
-
 (provide 'init-local)
 ;;; init-local.el ends here
