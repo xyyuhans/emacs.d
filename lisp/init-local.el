6b9a52a182e9a0a86025e7c72bb2134a538ce29e
xyyuhans <xyyuhans@hotmail.com>
2021-04-01 09:55:43 +0800

    disable auto save

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -213,14 +213,6 @@
   :config
   (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f")))
 
-;; auto save
-(use-package real-auto-save
-  :ensure t
-  :config
-  ;; no global mode now
-  (add-hook 'org-mode-hook 'real-auto-save-mode)
-  (add-hook 'markdown-mode-hook 'real-auto-save-mode)
-  (add-hook 'python-mode-hook 'real-auto-save-mode))
 
 (provide 'init-local)
 ;;; init-local.el ends here
